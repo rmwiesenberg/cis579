526e8aaa78470ddda72af4aa61d5b552031a4032
Ryan <rwiesenberg603@gmail.com>
2020-05-27 23:18:59 -0400

    nodes and breadth-first search complete

--- hw1/main.py
+++ hw1/main.py
@@ -1,69 +1,116 @@
 from typing import NamedTuple, Tuple, List
 
 
-class State(NamedTuple):
+class Node:
     """
-    State representing the river's state at a given point
-    origin_state: (missionaries, cultists) at origin side
-    target_state: (missionaries, cultists) at target side
-    boat_location: bool; T for Target, F for Origin
+    Node representing the river's state at a given point
     """
-    origin_state: Tuple[int, int]
-    target_state: Tuple[int, int]
-    boat_location: bool
-    attached_states: List['State'] = []
+
+    def __init__(
+            self,
+            origin_state: Tuple[int, int],
+            target_state: Tuple[int, int],
+            boat_location: bool
+    ):
+        """
+        origin_state: (missionaries, cultists) at origin side
+        target_state: (missionaries, cultists) at target side
+        boat_location: bool; T for Target, F for Origin
+        """
+
+        self.origin_state = origin_state
+        self.target_state = target_state
+        self.boat_location = boat_location
+        self.attached_nodes: List['Node'] = []
+
+    def __repr__(self):
+        str_out = (
+            f"({self.origin_state[0]}|{self.origin_state[1]}, "
+            f"{self.target_state[0]}|{self.target_state[1]}, "
+        )
+        if self.boat_location:
+            str_out += "T"
+        else:
+            str_out += "O"
+        return str_out + ")"
 
 
 # hand create state space diagram
-start_state = State((3, 3), (0, 0), False)
+start_state = Node((3, 3), (0, 0), False)
 
-n11 = State((2,2), (1,1), True)
-n12 = State((3,2), (0,1), True)
-n13 = State((3,1), (0,2), True)
+n11 = Node((2, 2), (1, 1), True)
+n12 = Node((3, 2), (0, 1), True)
+n13 = Node((3, 1), (0, 2), True)
 
-n21 = State((3,2), (0,1), False)
-n31 = State((3,0), (0,3), True)
-n41 = State((3,1), (0,2), False)
-n51 = State((1,1), (2,2), True)
-n61 = State((2,2), (1,1), False)
-n71 = State((0,2), (3,1), True)
-n81 = State((0,3), (3,0), False)
-n91 = State((0,1), (3,2), True)
+n21 = Node((3, 2), (0, 1), False)
+n31 = Node((3, 0), (0, 3), True)
+n41 = Node((3, 1), (0, 2), False)
+n51 = Node((1, 1), (2, 2), True)
+n61 = Node((2, 2), (1, 1), False)
+n71 = Node((0, 2), (3, 1), True)
+n81 = Node((0, 3), (3, 0), False)
+n91 = Node((0, 1), (3, 2), True)
 
-n101 = State((0,2), (3,1), False)
-n102 = State((0,1), (3,2), False)
-n103 = State((1,1), (2,2), False)
-goal_state = State((0, 0), (3, 3), True)
+n101 = Node((0, 2), (3, 1), False)
+n102 = Node((0, 1), (3, 2), False)
+n103 = Node((1, 1), (2, 2), False)
+goal_state = Node((0, 0), (3, 3), True)
 
-start_state.attached_states = [n11, n12, n13]
+start_state.attached_nodes = [n11, n12, n13]
 
-n11.attached_states = [start_state, n21]
-n12.attached_states = [start_state]
-n13.attached_states = [start_state, n21]
+n11.attached_nodes = [start_state, n21]
+n12.attached_nodes = [start_state]
+n13.attached_nodes = [start_state, n21]
 
-n21.attached_states = [n11, n13, n31]
-n31.attached_states = [n21, n41]
-n41.attached_states = [n31, n51]
-n51.attached_states = [n41, n61]
-n61.attached_states = [n51, n71]
-n71.attached_states = [n61, n81]
-n81.attached_states = [n71, n91]
-n91.attached_states = [n81, n101, n103]
+n21.attached_nodes = [n11, n13, n31]
+n31.attached_nodes = [n21, n41]
+n41.attached_nodes = [n31, n51]
+n51.attached_nodes = [n41, n61]
+n61.attached_nodes = [n51, n71]
+n71.attached_nodes = [n61, n81]
+n81.attached_nodes = [n71, n91]
+n91.attached_nodes = [n81, n101, n103]
 
-n101.attached_states = [n91, goal_state]
-n102.attached_states = [goal_state]
-n103.attached_states = [n91, goal_state]
+n101.attached_nodes = [n91, goal_state]
+n102.attached_nodes = [goal_state]
+n103.attached_nodes = [n91, goal_state]
 
-goal_state.attached_states = [n101, n102, n103]
+goal_state.attached_nodes = [n101, n102, n103]
 
 
 def breadth_first_search():
-    pass
+    print("Begin breadth-first")
+    visited_nodes = []
+    paths_to_check = [[start_state]]
+
+    first_goal_path = None
+    while paths_to_check:
+        path = paths_to_check.pop(0)
+        most_recent_node = path[-1]
+        if most_recent_node in visited_nodes:
+            continue
+        print(f"Checking {most_recent_node}")
+        visited_nodes.append(most_recent_node)
+        if most_recent_node == goal_state:
+            first_goal_path = path
+            print(f"Found goal node {most_recent_node}")
+            break
+        for attached_node in most_recent_node.attached_nodes:
+            if attached_node not in visited_nodes:
+                paths_to_check.append(path + [attached_node])
+    if not first_goal_path:
+        print("Did not find goal node")
+    else:
+        print(f"Final path: {first_goal_path}")
+    print("End breadth-first")
 
 
 def astar_search():
-    pass
+    print("Begin A*")
+
+    print("End A*")
 
 
 if __name__ == "__main__":
-    print(f"")
+    breadth_first_search()
+    astar_search()
